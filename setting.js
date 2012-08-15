e23d0e04e5b379f2e6db01cc69b38e80e600cc60
gopi_plus <gopi_plus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-15 13:51:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/onclick-popup/trunk@585770 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ setting.js
@@ -0,0 +1,55 @@
+/**
+ *     Onclick Popup
+ *     Copyright (C) 2012  www.gopiplus.com
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */	
+
+function onclickpopup_submit()
+{
+	if(document.onclickpopup_form.onclickpopup_group.value=="")
+	{
+		alert("Please enter the image path.")
+		document.onclickpopup_form.onclickpopup_group.focus();
+		return false;
+	}
+	else if(document.onclickpopup_form.onclickpopup_title.value=="")
+	{
+		alert("Please enter the popup title.")
+		document.onclickpopup_form.onclickpopup_title.focus();
+		document.onclickpopup_form.onclickpopup_title.select();
+		return false;
+	}
+	else if(document.onclickpopup_form.onclickpopup_content.value=="")
+	{
+		alert("Please enter the popup content.")
+		document.onclickpopup_form.onclickpopup_content.focus();
+		document.onclickpopup_form.onclickpopup_content.select();
+		return false;
+	}
+}
+
+function onclickpopup_delete(id)
+{
+	if(confirm("Do you want to delete this record?"))
+	{
+		document.frm_onclickpopup_display.action="admin.php?page=onclickpopup_admin_content&AC=DEL&DID="+id;
+		document.frm_onclickpopup_display.submit();
+	}
+}	
+
+function onclickpopup_redirect()
+{
+	window.location = "admin.php?page=onclickpopup_admin_content";
+}
